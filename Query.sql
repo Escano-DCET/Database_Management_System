513fef79deb47e91c7f127d186b09d88a6f6a2c9
Ferdinand C. Escaño, Jr. <jrescano8@gmail.com>
2026-02-21 16:58:02 +0800

    Add files via upload

--- Query.sql
+++ Query.sql
@@ -1,7 +1,7 @@
--- CREATE database mydbs;
+-- CREATE database mydb;
 -- show databases;
 
-use mydbs;
+use mydb;
 -- CREATE table customer
 -- (
 -- Cust_ID int NOT NULL,
@@ -13,7 +13,28 @@ use mydbs;
 -- );
 
 -- INSERT INTO customer (CUST_ID, Name, Address, Phone)
--- VALUES(11, 'John Smith', 'New York', 1234567);
+-- VALUES(1, 'John Smith', 'New York', 1234);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (2, 'Emily Johnson', 'Los Angeles', 5678 );
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (3, 'Michael Brown', 'Chicago', 9101);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (4, 'Jessica Davis', 'Houston', 1213);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (5, 'Daniel Wilson', 'Phoenix', 1415 );
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (6, 'Sarah Martinez', 'Philadelphia', 1617);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (7, 'David Anderson', 'San Antonio', 1819);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (8, 'Laura Thomas', 'San Diego', 2021);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (9, 'James Taylor', 'Dallas', 2223);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (10, 'Olivia Moore', 'San Jose', 2425);
+-- INSERT INTO customer (CUST_ID, Name, Address, Phone)
+-- VALUES (11, 'William Jackson', 'Austin', 2627);
+
 
 show tables;
 
