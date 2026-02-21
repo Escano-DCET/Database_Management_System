1c300c4100c8a9b706b2dec08b2a0f11664314e5
Ferdinand C. Escaño, Jr. <jrescano8@gmail.com>
2026-02-21 16:47:37 +0800

    Add files via upload

--- Query.sql
+++ Query.sql
@@ -1,15 +1,21 @@
-create database my;
+CREATE database mydbs;
 show databases;
 
-use my;
-create table customer
+use mydbs;
+CREATE table customer
 (
 Cust_ID int NOT NULL,
 Name char(50) NOT NULL,
-Adress char (50) NOT NULL,
+Address char (50) NOT NULL,
 Phone int NOT NULL,
 CONSTRAINT customer_pk PRIMARY KEY
 (Cust_ID)
 );
 
+INSERT INTO customer (CUST_ID, Name, Address, Phone)
+VALUES(11, 'John Smith', 'New York', 1234567);
+
 show tables;
+
+COMMIT;
+SELECT * FROM customer;
